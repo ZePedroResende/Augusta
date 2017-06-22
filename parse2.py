3da60c0e4d1650a50d1a794fc922ecdcc2fa24c2
ZePedroResede <j.p.resende68@gmail.com>
2017-06-22 22:53:30 +0100

    parse2.py

---
+++ parse2.py
@@ -0,0 +1,28 @@
+import sys, getopt
+import csv
+import json
+
+def main(argv):
+    input_file = 'file.csv'
+    output_file = 'file.json'
+    format = 'pretty'
+    read_csv(input_file, output_file, format)
+
+def read_csv(file, json_file, format):
+    csv_rows = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        title = reader.fieldnames
+        for row in reader:
+            csv_rows.extend([{title[i]:row[title[i]] for i in range(len(title))}])
+        write_json(csv_rows, json_file, format)
+
+def write_json(data, json_file, format):
+    with open(json_file, "w") as f:
+        if format == "pretty":
+            f.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': '),encoding="utf-8",ensure_ascii=False))
+        else:
+            f.write(json.dumps(data))
+
+if __name__ == "__main__":
+   main()
